c7f040b05950cb359a3adca52e87c5a43433cc27
Brian Moody <brian.kmdy@gmail.com>
2022-05-15 03:51:26 -0700

    Tweak settings in image recorder

--- imageRecorder.py
+++ imageRecorder.py
@@ -10,17 +10,24 @@ from PIL import ImageOps
 
 # Create the in-memory stream
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = (1024, 768)
 camera.start_preview()
+stream = BytesIO()
 time.sleep(2)
 
-while True:
-    stream = BytesIO()
-    camera.capture(stream, format='jpeg')
-    image = ImageOps.flip(Image.open(stream))
-    draw = ImageDraw.Draw(image)
-    myFont = ImageFont.truetype('cnr.otf', 32)
-    draw.text((28, 600), "Recording on\n{}".format(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")), font=myFont, fill=(255, 255, 255))
+try:
+    i = 1
+    while True:
+        start = time.time()
+        stream.seek(0)
+        camera.capture(stream, format='jpeg')
+        print('Captured image {} in time {:.2f}s'.format(i, time.time() - start))
+        image = ImageOps.flip(Image.open(stream))
+        draw = ImageDraw.Draw(image)
+        myFont = ImageFont.truetype('cnr.otf', 32)
+        draw.text((28, 600), "Recording on\n{}".format(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")), font=myFont, fill=(255, 255, 255))
 
-    image.save('capture.jpg'.format(int(time.time())))
-    time.sleep(0.1)
+        # image.save('capture-{}.jpg'.format(int(time.time())))
+        print('Saved image {} in time {:.2f}s'.format(i, time.time() - start))
+except KeyboardInterrupt:
+    pass
