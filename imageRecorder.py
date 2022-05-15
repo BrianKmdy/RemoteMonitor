23da42cf30af4aabd2984ebad8e7afd43ddff77d
Brian Moody <brian.kmdy@gmail.com>
2022-05-15 04:31:47 -0700

    More optimizations

--- imageRecorder.py
+++ imageRecorder.py
@@ -17,17 +17,18 @@ time.sleep(2)
 
 try:
     i = 1
-    while True:
-        start = time.time()
-        stream.seek(0)
-        camera.capture(stream, format='jpeg')
+    start = time.time()
+    for foo in camera.capture_continuous(stream, 'jpeg', burst=True):
         print('Captured image {} in time {:.2f}s'.format(i, time.time() - start))
         image = ImageOps.flip(Image.open(stream))
         draw = ImageDraw.Draw(image)
         myFont = ImageFont.truetype('cnr.otf', 32)
         draw.text((28, 600), "Recording on\n{}".format(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")), font=myFont, fill=(255, 255, 255))
 
-        # image.save('capture-{}.jpg'.format(int(time.time())))
+        image.save('capture.jpg')
         print('Saved image {} in time {:.2f}s'.format(i, time.time() - start))
+        i += 1
+        start = time.time()
+        stream.seek(0)
 except KeyboardInterrupt:
     pass
