5afe7aa99392653e9f9d162d2d8d4f3fad28ba49
Brian Moody <brian.kmdy@gmail.com>
2022-05-21 18:07:24 -0700

    Use websockets

--- remoteMonitor.py
+++ remoteMonitor.py
@@ -1,17 +1,33 @@
 import flask
-import flask_socketio
 import threading
 import imageRecorder
+import time
+import websockets
+import asyncio
 
 app = flask.Flask(__name__)
-socketio = flask_socketio.SocketIO(app)
 
 def broadcastThread():
     i = 0
     while True:
         image = imageRecorder.imageQueue.get()
-        socketio.emit('frame', {'index': i, 'bytes': image})
+        # socketio.emit('frame', {'index': i, 'bytes': image})
         i += 1
+        time.sleep(1)
+
+async def hello(websocket, path):
+    for message in websocket:
+        print(message)
+
+    for i in range(100):
+        print('Sending hello')
+        await websocket.send('hello {}'.format(i))
+        time.sleep(1)
+
+def websocketThread():
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    start_server = websockets.serve(hello, '0.0.0.0', 5001)
+    asyncio.get_event_loop().run_until_complete(start_server)
 
 @app.route('/')
 def home():
@@ -24,16 +40,10 @@ def capture():
         return flask.Response(imageRecorder.imageQueue.get(), mimetype="image/jpg")
     return flask.Response()
 
-@socketio.on('connect')
-def connect(auth):
-    print('User connected')
-
-@socketio.on('disconnect')
-def disconnect():
-    print('Client disconnected')
-
 if __name__ == '__main__':
-    threading.Thread(target=imageRecorder.capture, daemon=True).start()
+    # threading.Thread(target=imageRecorder.capture, daemon=True).start()
     threading.Thread(target=broadcastThread, daemon=True).start()
-    socketio.run(app, host='0.0.0.0')
-    # app.run(host='0.0.0.0')
+    threading.Thread(target=websocketThread, daemon=True).start()
+    
+
+    app.run(host='0.0.0.0')
