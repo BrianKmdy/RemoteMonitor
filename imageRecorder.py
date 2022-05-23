072789118619e6b0fe387c346ef44eb2afad2ed6
Brian Moody <brian.kmdy@gmail.com>
2022-05-22 18:29:14 -0700

    Save images to folder for http server

--- imageRecorder.py
+++ imageRecorder.py
@@ -1,22 +1,26 @@
+import humidityReader
 from io import BytesIO
 import time
 import datetime
 from picamera import PiCamera
 import collections
-import humidityReader
 
-frameRate = 2
 frameCount = 0
 bufferSeconds = 10
+period = 15 # Length of period in seconds
 
-maxQueueSize = int(frameRate * bufferSeconds)
+maxQueueSize = 10
 imageQueue = collections.deque(maxlen=maxQueueSize)
 
 def getOverlayText():
-    return "{}\n{:.1f}C   {:.1f}%".format(
+    return "{}\nTemp: {:.1f}C  Humidity: {:.1f}%".format(
         datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
         humidityReader.temperature,
-        humidityReader.temperature)
+        humidityReader.humidity)
+        
+def getStartOfPeriod():
+    global period
+    return int(time.time() / period) * period
 
 def capture():
     print('Capture thread started')
@@ -32,23 +36,22 @@ def capture():
     camera.vflip = True
     camera.resolution = (1440, 1080)
     camera.annotate_text_size = 20
-    camera.framerate = frameRate
     camera.annotate_text = getOverlayText()
     camera.start_preview()
     time.sleep(2)
 
     try:
         start = time.time()
-        for frame in camera.capture_continuous(imageBytes, 'jpeg', burst=True, quality=50):
+        for frame in camera.capture_continuous(imageBytes, 'jpeg', burst=True, quality=35):
             print('Captured image {} in time {:.2f}s'.format(frameCount, time.time() - start))
             imageQueue.append(imageBytes.getvalue())
-            print('Queue size: {}'.format(len(imageQueue)))
-            print('Saved image {} in time {:.2f}s'.format(frameCount, time.time() - start))
 
             frameCount += 1
-            start = time.time()
             imageBytes.seek(0)
             imageBytes.truncate()
+
+            time.sleep(getStartOfPeriod() + period - time.time())
             camera.annotate_text = getOverlayText()
+            start = time.time()
     except KeyboardInterrupt:
         pass
--- remoteMonitor.py
+++ remoteMonitor.py
@@ -1,65 +1,39 @@
-import flask
-import flask_socketio
-import threading
 import imageRecorder
-import time
 import humidityReader
+import threading
+import time
+import os
+import datetime
 
-app = flask.Flask(__name__)
-socketio = flask_socketio.SocketIO(app)
+captureDirectory = '/home/pi/captures'
 
-lastAck = 0
-bufferSeconds = 5
-frameIndex = 0
+def getCaptureName():
+    return datetime.datetime.now().strftime("%Y-%m-%d/%H/%Y-%m-%d_%H:%M:%S.jpg")
 
-clientsConnected = 0
-
-def broadcastThread():
-    global lastAck
-    global bufferSeconds
-    global frameIndex
-    global clientsConnected
+def saveCaptures():
+    global period
+    os.makedirs(captureDirectory, exist_ok=True)
+    frameIndex = 0
+    currentPeriod = 0
     while True:
-        if clientsConnected < 1:
-            time.sleep(0.1)
-            continue
-
-        print('Comparison: {} and {}'.format(frameIndex - lastAck, bufferSeconds * imageRecorder.frameRate))
-        if len(imageRecorder.imageQueue) > 0 and frameIndex - lastAck < bufferSeconds * imageRecorder.frameRate:
+        if len(imageRecorder.imageQueue) > 0:
             image = imageRecorder.imageQueue.popleft()
-            print('Sending frame {}'.format(frameIndex))
-            socketio.emit('frame', {'num': frameIndex, 'bytes': image})
-            frameIndex += 1
+            if imageRecorder.getStartOfPeriod() != currentPeriod:
+                currentPeriod = imageRecorder.getStartOfPeriod()
+                capturePath = os.path.join(captureDirectory, getCaptureName())
+                print('Saving image {}'.format(capturePath))
+                if not os.path.exists(os.path.dirname(capturePath)):
+                    os.makedirs(os.path.dirname(capturePath), exist_ok=True)
+                with open(capturePath, 'wb') as f:
+                    f.write(image)
+                frameIndex += 1
         else:
-            time.sleep(0.05)
-
-@app.route('/')
-def home():
-    return flask.render_template('index.html')
-
-@socketio.on('connect')
-def connect(auth):
-    global clientsConnected
-    global frameIndex
-    global lastAck
-    clientsConnected += 1
-    lastAck = frameIndex
-    print('User connected')
-
-@socketio.on('disconnect')
-def disconnect():
-    global clientsConnected
-    clientsConnected -= 1
-    print('Client disconnected')
-
-@socketio.on('ack')
-def handle_json(json):
-    global lastAck
-    print('Received ack ' + str(json))
-    lastAck = max(lastAck, json['num'])
+            time.sleep(0.1)
 
 if __name__ == '__main__':
-    threading.Thread(target=imageRecorder.capture, daemon=True).start()
-    threading.Thread(target=broadcastThread, daemon=True).start()
-    threading.Thread(target=humidityReader.getHumidityData, daemon=True).start()
-    socketio.run(app, host='0.0.0.0')
+    try:
+        threading.Thread(target=imageRecorder.capture, daemon=True).start()
+        threading.Thread(target=humidityReader.getHumidityData, daemon=True).start()
+        saveCaptures()
+    except KeyboardInterrupt:
+        pass
